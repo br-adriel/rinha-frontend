7f6d12e20cbb3eb0199dd66f637e7e5c73ea1281
Adriel <adriel.fsantos@outlook.com>
2023-10-11 14:55:13 -0300

    Insert FileInput on Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
-import style from './home.module.css';
+import FileInput from '../../components/FileInput';
+import style from './Home.module.css';
 
 interface Props {
   invalid?: boolean
@@ -9,7 +10,7 @@ function Home({ invalid = false }: Props) {
     <main className={style.main}>
       <h1 className='text-center'>JSON Tree Viewer</h1>
       <h2 className='text-center'>Simple JSON Viewer that runs completely on-client. No data exchange</h2>
-      <button type='button'>Load JSON</button>
+      <FileInput>Load JSON</FileInput>
       {invalid && <p className={style.error}>Invalid file. Please load a valid JSON file.</p>}
     </main>
   )
