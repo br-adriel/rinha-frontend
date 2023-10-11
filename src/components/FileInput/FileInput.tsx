f93ffc3cf0b44d39caf24790c7215102cf944bf8
Adriel <adriel.fsantos@outlook.com>
2023-10-11 15:04:34 -0300

    Implement prop onFileSelected

--- src/components/FileInput/FileInput.tsx
+++ src/components/FileInput/FileInput.tsx
@@ -1,9 +1,11 @@
 import { PropsWithChildren, useRef } from "react";
 import style from './FileInput.module.css';
 
-interface Props extends PropsWithChildren { }
+interface Props extends PropsWithChildren {
+  onFileSelected: (files: FileList | null) => void
+}
 
-const FileInput = ({ children }: Props) => {
+const FileInput = ({ children, onFileSelected }: Props) => {
   const hiddenFileInput = useRef<HTMLInputElement>(null)
 
   const onClick = () => {
@@ -12,11 +14,17 @@ const FileInput = ({ children }: Props) => {
     }
   }
 
+  const hiddenFileInputChange = () => {
+    if (hiddenFileInput.current) {
+      onFileSelected(hiddenFileInput.current.files)
+    }
+  }
+
   return <>
     <button onClick={onClick}>
       {children}
     </button>
-    <input type="file" ref={hiddenFileInput} className={style.fileInput} />
+    <input type="file" ref={hiddenFileInput} className={style.fileInput} onChange={hiddenFileInputChange} />
   </>
 }
 
