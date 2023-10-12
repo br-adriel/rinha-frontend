1f664a526d1f3390a7c45b44fdad825b35e26840
Adriel <adriel.fsantos@outlook.com>
2023-10-12 00:25:25 -0300

    Update Home page to use JsonContext

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,36 @@
 import FileInput from '../../components/FileInput';
+import { useJsonContext } from '../../contexts/JsonContext';
 import style from './Home.module.css';
 
 interface Props {
-  invalid?: boolean
+  invalid?: boolean;
 }
 
 function Home({ invalid = false }: Props) {
+  const { setHasFile, setJsonObject } = useJsonContext();
+
+  const fileSelect = async (files: FileList | null) => {
+    if (files && files.length == 1) {
+      const content = JSON.parse((await files.item(0)?.text()) || '');
+      setHasFile(true);
+      setJsonObject(content);
+    }
+  };
+
   return (
     <main className={style.main}>
       <h1 className='text-center'>JSON Tree Viewer</h1>
-      <h2 className='text-center'>Simple JSON Viewer that runs completely on-client. No data exchange</h2>
-      <FileInput>Load JSON</FileInput>
-      {invalid && <p className={style.error}>Invalid file. Please load a valid JSON file.</p>}
+      <h2 className='text-center'>
+        Simple JSON Viewer that runs completely on-client. No data exchange
+      </h2>
+      <FileInput onFileSelected={fileSelect}>Load JSON</FileInput>
+      {invalid && (
+        <p className={style.error}>
+          Invalid file. Please load a valid JSON file.
+        </p>
+      )}
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
