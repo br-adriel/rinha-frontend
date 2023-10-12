e9353437863530946f3942f87eade1e296c51072
Adriel <adriel.fsantos@outlook.com>
2023-10-12 17:41:09 -0300

    Add Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,17 @@
+function Loading() {
+  return (
+    <main
+      style={{
+        padding: '24px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      <h1 style={{ textAlign: 'center' }}>Loading...</h1>
+    </main>
+  );
+}
+
+export default Loading;
