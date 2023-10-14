92cebb955efd415048912c9e7d73cf5ea94ff9e1
Adriel <adriel.fsantos@outlook.com>
2023-10-14 00:58:03 -0300

    Insert Footer on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import { Suspense, lazy } from 'react';
 import Loading from './components/Loading';
 import { useJsonContext } from './contexts/JsonContext';
+import Footer from './components/Footer';
 const Home = lazy(() => import('./pages/Home'));
 const JsonViewer = lazy(() => import('./pages/JsonViewer'));
 
 function App() {
   const { file } = useJsonContext();
 
-  return file ? (
-    <Suspense fallback={<Loading />}>
-      <JsonViewer />
-    </Suspense>
-  ) : (
-    <Suspense fallback={<Loading />}>
-      <Home />
-    </Suspense>
+  return (
+    <>
+      {file ? (
+        <Suspense fallback={<Loading />}>
+          <JsonViewer />
+        </Suspense>
+      ) : (
+        <Suspense fallback={<Loading />}>
+          <Home />
+          <Footer />
+        </Suspense>
+      )}
+      <Footer />
+    </>
   );
 }
 
