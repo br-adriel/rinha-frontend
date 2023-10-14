f71dabb0b27a8c8c25eaaa39c4fa6dd2b30ac280
Adriel <adriel.fsantos@outlook.com>
2023-10-14 00:12:22 -0300

    Update FileInput for better acessibility

--- src/components/FileInput/FileInput.tsx
+++ src/components/FileInput/FileInput.tsx
@@ -1,32 +1,43 @@
-import { PropsWithChildren, useRef } from "react";
+import { PropsWithChildren, useRef } from 'react';
 import style from './FileInput.module.css';
 
 interface Props extends PropsWithChildren {
-  onFileSelected: (files: FileList | null) => void
+  onFileSelected: (files: FileList | null) => void;
 }
 
 const FileInput = ({ children, onFileSelected }: Props) => {
-  const hiddenFileInput = useRef<HTMLInputElement>(null)
+  const hiddenFileInput = useRef<HTMLInputElement>(null);
 
   const onClick = () => {
     if (hiddenFileInput.current) {
-      hiddenFileInput.current.click()
+      hiddenFileInput.current.click();
     }
-  }
+  };
 
   const hiddenFileInputChange = () => {
     if (hiddenFileInput.current) {
-      onFileSelected(hiddenFileInput.current.files)
+      onFileSelected(hiddenFileInput.current.files);
     }
-  }
+  };
 
-  return <>
-    <button onClick={onClick}>
-      {children}
-    </button>
-    <input type="file" ref={hiddenFileInput} className={style.fileInput} onChange={hiddenFileInputChange} accept=".json" />
-  </>
-}
-
+  return (
+    <>
+      <button onClick={onClick} type='button'>
+        {children}
+      </button>
+      <label htmlFor='file' style={{ display: 'none' }}>
+        Open JSON File
+      </label>
+      <input
+        id='file'
+        type='file'
+        ref={hiddenFileInput}
+        className={style.fileInput}
+        onChange={hiddenFileInputChange}
+        accept='.json'
+      />
+    </>
+  );
+};
 
 export default FileInput;
